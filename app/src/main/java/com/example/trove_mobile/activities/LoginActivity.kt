da4523115f304f4c8b5652180cbac87f206c09af
Thareendra2000 <thareendra2000@gmail.com>
2024-09-29 11:58:12 +0530

    Login Activity added

---
+++ app/src/main/java/com/example/trove_mobile/activities/LoginActivity.kt
@@ -0,0 +1,21 @@
+package com.example.trove_mobile.activities
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.trove_mobile.R
+
+class LoginActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.login_page)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.login)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
