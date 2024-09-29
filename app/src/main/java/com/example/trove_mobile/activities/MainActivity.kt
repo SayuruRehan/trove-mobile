85ccd7e543187433b0c83bf9be7607f847e9ba98
Thareendra2000 <thareendra2000@gmail.com>
2024-09-29 11:46:35 +0530

    Project setup finalized

--- app/src/main/java/com/example/trove_mobile/activities/MainActivity.kt
+++ app/src/main/java/com/example/trove_mobile/activities/MainActivity.kt
@@ -1,10 +1,13 @@
-package com.example.trove_mobile
+package com.example.trove_mobile.activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.trove_mobile.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +18,14 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        // Get reference to the Get Started button
+        val getStartedButton = findViewById<Button>(R.id.button_get_started)
+        // Set an OnClickListener on the button
+        getStartedButton.setOnClickListener {
+            // Start the LoginActivity
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
         }
     }
 }
